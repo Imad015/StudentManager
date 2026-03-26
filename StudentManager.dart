8d6d5b3ffdaa9487c3c387ae36e8196615c79f51
Moulay Imad <111585460+Imad015@users.noreply.github.com>
2026-03-26 14:52:47 +0100

    first commit

---
+++ StudentManager.dart
@@ -0,0 +1,10 @@
+class Studentmanager {
+  String fullName;
+  int numberStudent;
+  double grade;
+  Studentmanager({
+    required this.fullName,
+    required this.numberStudent,
+    required this.grade,
+  });
+}
---
+++ main.dart
@@ -0,0 +1,131 @@
+import 'StudentManager.dart';
+import 'dart:io';
+
+void main() {
+  List<Studentmanager> Students = [];
+  print("Hello in Student Manager");
+
+  while (true) {
+    print("""\nEnter number 1 to add new student,
+      \n enter 2 to show list students,
+      \n enter 3 to add grade\n
+      \n enter 4 to remove student\n
+      enter 0 to exit""");
+    String? inputUser = stdin.readLineSync();
+    if (inputUser == '1') {
+      _addStudent(Students);
+      print("Enter successfully");
+    } else if (inputUser == '2') {
+      _showListStudent(Students);
+    } else if (inputUser == '3') {
+      _changeGrade(Students);
+    } else if (inputUser == '0') {
+      break;
+    } else if (inputUser == '4') {
+      _removeStudent(Students);
+    } else {
+      print("Please enter number True!");
+    }
+  }
+}
+
+//===== ADD STUDENT=============
+void _addStudent(List<Studentmanager> Students) {
+  stdout.write("enter full name to Student: ");
+  String? fullNameStudent = stdin.readLineSync();
+  if (fullNameStudent != null && fullNameStudent.isNotEmpty) {
+    print("Entering Succeess");
+  } else {
+    print("Please enter the name student true");
+  }
+  stdout.write("enter to number Student: ");
+  //Enter number
+  String? inputNumberStudent = stdin.readLineSync();
+  int? numberStudent;
+  if (inputNumberStudent != null && inputNumberStudent.isNotEmpty) {
+    numberStudent = int.tryParse(inputNumberStudent);
+    if (numberStudent == null) {
+      print("Please enter a valid integer for student number");
+      return;
+    }
+  } else {
+    print("Please enter the student number");
+    return;
+  }
+  //add grade
+  stdout.write("enter grade: ");
+  String? inputGrade = stdin.readLineSync();
+  double? grade;
+  if (inputGrade != null && inputGrade.isNotEmpty) {
+    grade = double.tryParse(inputGrade);
+    if (grade == null) {
+      print("Please enter a valid number for grade");
+      return;
+    }
+  } else {
+    print("Please enter the grade");
+    return;
+  }
+  Students.add(
+    Studentmanager(
+      fullName: fullNameStudent!,
+      numberStudent: numberStudent,
+      grade: grade,
+    ),
+  );
+}
+
+void _showListStudent(List<Studentmanager> Students) {
+  if (Students.isEmpty) {
+    print("List is empty");
+    return;
+  }
+  for (int i = 0; i < Students.length; i++) {
+    print(
+      "Student number ${Students[i].numberStudent},Fullname:${Students[i].fullName},Grade:${Students[i].grade}",
+    );
+  }
+}
+
+// =====ADD GREADE===========
+void _changeGrade(List<Studentmanager> Students) {
+  stdout.write("enter number student: ");
+  String? inputNumberStudent = stdin.readLineSync();
+  int? numberGrade = int.tryParse(inputNumberStudent ?? "");
+  if (numberGrade != null && numberGrade >= 0) {
+    for (var i = 0; i < Students.length; i++) {
+      if (numberGrade == Students[i].numberStudent) {
+        print("enter new Grade: ");
+        String? inputNewGrade = stdin.readLineSync();
+        double? newGrade = double.tryParse(inputNewGrade ?? "");
+        if(newGrade!=null&&newGrade>=0){
+          Students[i].grade = newGrade;
+        }
+        
+      } else {
+        print("the number not found!");
+      }
+    }
+  }
+}
+
+void _removeStudent(List<Studentmanager> Students) {
+  if (Students.isEmpty) {
+    print("List is empty");
+    return;
+  }
+
+  stdout.write("enter number Student to romove: ");
+  String? inputStudentToRemove = stdin.readLineSync();
+  final numberStudentToRemove = int.tryParse(inputStudentToRemove ?? "");
+  if (numberStudentToRemove != null && numberStudentToRemove >= 0) {
+    for (var i = 0; i < Students.length; i++) {
+      if (numberStudentToRemove == Students[i].numberStudent) {
+        Students.remove(Students[i]);
+        print("Remove complite");
+      } else {
+        print("The number Not Find");
+      }
+    }
+  }
+}
